f0f3aea5c45a2e6aeb2a81b3517d8010c289323f
siwel20 <plthrock@gmail.com>
2020-05-13 19:23:42 -0600

    added operators to the game

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,10 +1,31 @@
-// this creates a function named "fight"
 var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
 
-console.log(playerName);
+// You can log multiple values at once like this 
+console.log(playerName, playerAttack, playerHealth);
 
-function fight() {
-    window.alert("The fight has begun!");
-}
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
-// fight();
+var fight = function() {
+    // alert users that they are starting the round
+    window.alert("Welcome to Robot Gladiators!");
+
+    // Subtract the value of 'playerAttack' from the value of 'enemyHealth' and use that result to update the value in the 'enemyHealth' variable
+    enemyHealth = enemyHealth - playerAttack;
+    // Log a resulting messge to the console so we know that it worked.
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+    );
+
+    // Subtract the value of 'enemyAttack' from the value of 'playerHealth' and
+    playerHealth = playerHealth - enemyAttack;
+    // Log a resulting message to the console so we know that it worked. 
+    console.log (
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaing."
+    );
+};
+
+fight();
