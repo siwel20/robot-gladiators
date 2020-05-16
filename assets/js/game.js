5206bf8f615b86ad6b3b42544c73e55c9bb47aea
siwel20 <plthrock@gmail.com>
2020-05-16 17:03:59 -0600

    fixed bug fight-skip

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,32 +1,34 @@
 
+var fightOrSkip = function() {
+
+  var promptFight = window.prompt('Would you like FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
+
+  // Conditional Recursive Function Call
+  if (promptFight === "" || promptFight === null) {
+    window.alert("You need to provide a valid answer! Please try again");
+    return fightOrSkip();
+  }
+  promptFight = promptFight.toLowerCase();
+  // if user picks "skip" confirm and then stop the loop
+  if (promptFight === "skip" || promptFight === "SKIP") {
+    // confirm user wants to skip
+    var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+    // if yes (true), leave fight
+    if (confirmSkip) {
+      window.alert(playerInfo.name + ' has decided to skip this fight. Goodbye!');
+      // subtract money from playerInfo.money for skipping
+      playerInfo.money = Math.max(0, playerInfo.money - 10);
+      return true;
+    }
+  }
+};
 
 var fight = function(enemy) {
-    while (playerInfo.health > 0 && enemy.health > 0) {
-      // ask user if they'd liked to fight or run
-      var promptFight = window.prompt('Would you like FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
-      if (promptFight === "fight" || promptFight === "FIGHT") {
-        console.log("Fight");
+    while(enemy.health > 0 && playerInfo.health > 0) {
+      if (fightOrSkip()) {
+        break;
       }
-  
-      // if user picks "skip" confirm and then stop the loop
-      else if (promptFight === "skip" || promptFight === "SKIP") {
-        // confirm user wants to skip
-        var confirmSkip = window.confirm("Are you sure you'd like to quit?");
-  
-        // if yes (true), leave fight
-        if (confirmSkip) {
-          window.alert(playerInfo.name + ' has decided to skip this fight. Goodbye!');
-          // subtract money from playerInfo.money for skipping
-          playerInfo.money = Math.max(0, playerInfo.money - 10);
-          console.log("playerInfo.money", playerInfo.money);
-          break;
-        }
-      }
-      else {
-        window.alert("You need to pick a valid option. Try again!");
-        fight();
-      }
-  
       // remove enemy's health by subtracting the amount set in the playerInfo.attack variable
       var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
 
